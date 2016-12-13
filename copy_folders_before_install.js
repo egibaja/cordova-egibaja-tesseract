ddbb45ad5f3e3210c28785217fc173217a06f518
Eduardo Gibaja <eduardogibaja@gmail.com>
2016-12-13 17:56:56 +0100

    using different npm module

--- copy_folders_before_install.js
+++ copy_folders_before_install.js
@@ -1,33 +1,31 @@
-var fs = require("fs");
 var path = require("path");
 var rootdir = __dirname;
 console.log(rootdir);
 
 console.log("Running hook: " + path.basename(__filename));
 
-var ncp = require('ncp').ncp;
-
-ncp.limit = 16;
+var fs = require('fs.extra');
  
 var lib_source = path.join(rootdir,'libs');
 var lib_dest = path.join(rootdir,'..', '..', 'platforms', 'android', 'libs');
 
 console.log(lib_dest)
 
-ncp(lib_source, lib_dest, function (err) {
+fs.copyRecursive(lib_source, lib_dest, function (err) {
  if (err) {
    return console.error(err);
  }
  console.log('lib done!');
 });
 
-var tessdata_source = path.join(rootdir,'tessdata','');
+var tessdata_source = path.join(rootdir,'tessdata');
 var tessdata_dest = path.join(rootdir,'..', '..', 'platforms', 'android', 'assets');
-
+console.log("Tessdata source and dest")
+console.log(tessdata_source)
 console.log(tessdata_dest)
 
  
-ncp(tessdata_source, tessdata_dest, function (err) {
+fs.copyRecursive(tessdata_source, tessdata_dest, function (err) {
  if (err) {
    return console.error(err);
  }
