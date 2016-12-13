ceb36c64ce8ab85621056d1e931a607eb9687a77
Eduardo Gibaja <eduardogibaja@gmail.com>
2016-12-13 17:37:13 +0100

    copy folders before installing

--- copy_folders_before_install.js
+++ copy_folders_before_install.js
@@ -4,3 +4,25 @@ var rootdir = __dirname;
 console.log(rootdir);
 
 console.log("Running hook: " + path.basename(__filename));
+
+ncp.limit = 16;
+ 
+var lib_source = path.join(rootdir,'libs','*');
+var lib_dest = path.join(rootdir,'..', '..', 'platforms', 'android', 'libs')
+
+ncp(lib_source, lib_dest, function (err) {
+ if (err) {
+   return console.error(err);
+ }
+ console.log('lib done!');
+});
+
+var tessdata_source = path.join(rootdir,'tessdata');
+var tessdata_dest = path.join(rootdir,'..', '..', 'platforms', 'android', 'assets')
+ 
+ncp(tessdata_source, tessdata_dest, function (err) {
+ if (err) {
+   return console.error(err);
+ }
+ console.log('tessdata done!');
+});
