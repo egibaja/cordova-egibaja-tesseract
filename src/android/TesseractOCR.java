ebc66bb4fad20f4644386a69427d6115d8159257
Eduardo Gibaja <eduardogibaja@gmail.com>
2016-12-21 12:47:49 +0100

    binarize image

--- src/android/TesseractOCR.java
+++ src/android/TesseractOCR.java
@@ -168,8 +168,8 @@ public class TesseractOCR extends CordovaPlugin {
       Log.e(TAG, "Dimension de la imagen width "+ imageWidth+ ", height "+ imageHeight);
 
       try {
-        //bitmap = createBinaryImage(bitmap);
-        //bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        bitmap = createBinaryImage(bitmap);
+        bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
         bitmap = Bitmap.createScaledBitmap(bitmap, imageWidth * 2, imageHeight * 2, false);
 
       // scan and recognize the bitmap image
